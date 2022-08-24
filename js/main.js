7d110dc43aab99889401236c50f6b87be3af25c4
jpfingsten <jasmine.pfingsten@gmail.com>
2022-08-24 16:21:13 -0400

    Begin adding modal

--- js/main.js
+++ js/main.js
@@ -7,4 +7,18 @@ const navbar = document.querySelector('nav');
     } else {
   navbar.classList.remove('nav-active');
     }
-  };
+};
+
+// Opening and closing family picture modal
+
+const modal = document.querySelector("#modal");
+const openModal = document.getElementById("modal-link");
+const closeModal = document.querySelector(".close-button");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
