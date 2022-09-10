a9b28dac9ee9a47ab830bfe0a2d5c27e0edf3ed6
jasmine_p <jasmine.pfingsten@gmail.com>
2022-09-10 06:53:14 -0400

    Begin nav rework, remove contact button styles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Fixed nav bar transition
 
-const navbar = document.querySelector('nav');
-  window.onscroll = () => {
-    if (window.scrollY > navbar.clientHeight) {
-  navbar.classList.add('nav-active');
-    } else {
-  navbar.classList.remove('nav-active');
-    }
-};
+// const navbar = document.querySelector('nav');
+//   window.onscroll = () => {
+//     if (window.scrollY > navbar.clientHeight) {
+//   navbar.classList.add('nav-active');
+//     } else {
+//   navbar.classList.remove('nav-active');
+//     }
+// };
 
 // Opening and closing family picture modal
 
