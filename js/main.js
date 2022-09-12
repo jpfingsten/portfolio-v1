ecd569ed7cc3eba9dc8c5f5b1d15b943ba57cc9c
jasmine_p <jasmine.pfingsten@gmail.com>
2022-09-12 06:37:08 -0400

    Hamburger menu functional, style tweaks forthcoming

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 // Fixed nav bar transition
 
-// const navbar = document.querySelector('nav');
-//   window.onscroll = () => {
-//     if (window.scrollY > navbar.clientHeight) {
-//   navbar.classList.add('nav-active');
-//     } else {
-//   navbar.classList.remove('nav-active');
-//     }
-// };
+const navbar = document.querySelector('.navbar');
+
+  window.onscroll = () => {
+    if (window.scrollY > navbar.clientHeight) {
+  navbar.classList.add('nav-active');
+    } else {
+  navbar.classList.remove('nav-active');
+    }
+};
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelector(".nav-link");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
 
 // Opening and closing family picture modal
 
